4695c8c88c90c598f8a3be6a5a2b02e21bbf9ae1
Lindsay Nickalo <bradleyl4@mailbox.winthrop.edu>
2019-12-02 20:13:13 +0000

    Hello World in NetBeans

---
+++ HelloWorld/HelloWorld/src/main/java/Hello.java
@@ -0,0 +1,13 @@
+/**
+ *
+ * @author Lindsay Gen10
+ * Date: December 02, 2019
+ * Hello World practice program to gain familiarity with NetBeans
+ */
+package mrsnickalo.helloworld;
+public class Hello {
+    public static void main(String[] args){
+        System.out.println("Hello, World!");
+        System.out.println("It's Harry Potter!");
+    }
+}
